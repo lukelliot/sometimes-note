aed9771eed03e6b7251676f1d68ebc289dbce3c3
lukelliot <lucaselliotchin@gmail.com>
2016-08-02 14:55:39 -0400

    Add static_pages_controller

--- app/models/api/user.rb
+++ app/models/api/user.rb
@@ -3,6 +3,9 @@ class Api::User < ActiveRecord::Base
   validates :password_digest, presence: true
   validates :session_token, :username, presence: true, uniqueness: true
 
+  # has_many :notes
+  # has_many :notebooks
+
   after_initialize :ensure_logged_in
 
   attr_reader :password
@@ -18,6 +21,29 @@ class Api::User < ActiveRecord::Base
   end
 
   def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
+  def reset_session_token!
+    self.session_token = generate_session_token!
+    self.save!
+    self.session_token
+  end
+
+  def generate_session_token!
+    token = SecureRandom::urlsafe_base64(16)
+
+    while User.exists(session_token: token)
+      token = SecureRandom::urlsafe_base64(16)
+    end
+
+    token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token!
   end
 end
