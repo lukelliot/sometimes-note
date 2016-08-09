336cac16e3e789815d9a6ff17832835ad16ec711
lukelliot <lucaselliotchin@gmail.com>
2016-08-09 11:23:17 -0400

    Completed Notebooks Controller

---
+++ app/controllers/api/notebooks_controller.rb
@@ -0,0 +1,45 @@
+class Api::NotebooksController < ApplicationController
+  def index
+    @notebooks = current_user.notebooks
+    render :index
+  end
+
+  def create
+    @notebook = current_user.notebooks.new(notebooks_params)
+
+    if @notebook.save
+      render :show
+    else
+      render json: @notbeook.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @notebook = current_user.notebooks.find(params[:id])
+
+    if @notebook.update_attributes(notebooks_params)
+      render :show
+    else
+      render json: @notebook.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @notebook = current_user.notebooks.find(params[:id])
+
+    if @notebook.destroy
+      render :show
+    else
+      render json: ["Could not delete #{@notebook.title}"], status: 403
+    end
+  end
+
+  private
+
+  def notebooks_params
+    params.require(:notebook).permit(
+      :title,
+      :description,
+    )
+  end
+end
